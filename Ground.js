d0988881bf1d6aac37eb25eee324517436c1c5fb
LFFALAN <72618838+LFFALAN@users.noreply.github.com>
2021-01-20 20:47:08 +0530

    Update Ground.js

--- Ground.js
+++ Ground.js
@@ -1,25 +1,19 @@
 
 class Ground{
   constructor(x,y,width,height){
-      var options = {
-      isStatic:true
+      var options={
+          isStatic:true
       }
-  this.x = x;
-  this.y = y;
-  this.width = width;
-  this.height = height;
-  this.body = Bodies.rectangle(x,y,this.width, this.height, options);
-  World.add(world, this.body);
-  }
-  display(){
-  var pos = this.body.position;
-  push();
-  translate(pos.x, pos.y);
-  rectMode(CENTER);
-  fill("white")
-  rect(0,0,this.width,this.height);
-  pop();
   
-  }
-  
-  }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height=height;
+      World.add(world,this.body)
+      }
+      display(){
+          var pos = this.body.position;
+          rectMode(CENTER);
+          fill("white");
+          rect(pos.x,pos.y,this.width,this.height)
+      }
+}
